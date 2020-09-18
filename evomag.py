c971f88263aacbc2e2f240d9e53c2cca7e4dbfe6
Loghin Alexandru <loghinalexandru61@gmail.com>
2020-09-19 01:42:06 +0300

    Add files via upload

--- evomag.py
+++ evomag.py
@@ -12,11 +12,11 @@ def crawl_evomag():
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
     }
 
-    # smtp = smtplib.SMTP('smtp.gmail.com', 587)
-    # smtp.ehlo()
-    # smtp.starttls()
-    # smtp.ehlo()
-    # smtp.login(sender_email, '')
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(sender_email, '')
 
     target = 'https://www.evomag.ro/componente-pc-gaming-placi-video/asus-placa-video-asus-geforce-rtx-3080-tuf-gaming-10gb-gddr6x-320-bit-3798493.html'
 
